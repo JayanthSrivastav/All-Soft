e5673a45da1f73179cb3f6da31830ba4d6115343
JayanthSrivastav <66516111+JayanthSrivastav@users.noreply.github.com>
2024-10-14 22:35:32 +0530

    Update MongoDB.js

--- src/subs/MongoDB.js
+++ src/subs/MongoDB.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Box } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import '../App.css';
 
 const MongoDB = () => {
   return (
@@ -8,10 +11,7 @@ const MongoDB = () => {
         <div className='box-div'>
           <div className='Tool' >
             <a href='https://docs.aws.amazon.com/corretto/latest/corretto-8-ug/downloads-list.html' target='_blank' rel="noreferrer">
-              <img src='https://venturebeat.com/wp-content/uploads/2015/12/oracle-java-e1450723340931.jpg?w=1200&strip=all' alt='java' width='40%' className='IMU' ></img>
-              {/* <Typography variant='h1' className='im-head'>
-                java
-              </Typography> */}
+              <img src='' alt='mongodb' width='40%' className='IMU' ></img>
             </a>
             <Typography variant='body'>
 
@@ -28,7 +28,7 @@ const MongoDB = () => {
             </Typography>
             <p className='box-text'>Go to <i>MongoDB</i>'s Community Edition <a href='https://www.mongodb.com/try/download/community' target='_blank' rel="noreferrer">website</a> to download.</p>
             <img src={require('../assets/mongo/compassandmongodbcommunity/download-step1.png')} alt='step-1' className='screenshots'/>
-
+            <Divider className='custom-divider'/>
             <p className='box-text'>Scroll down. Select the latest version, platform and download the msi package.</p>
             <img src={require('../assets/mongo/compassandmongodbcommunity/download-step1-1.png')} alt='step-1-1' className='screenshots'/>
 
@@ -41,43 +41,60 @@ const MongoDB = () => {
             </Typography>
             <p className='box-text'>Open the installer.</p>
             <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step2.png')} alt='step-2' className='screenshots' />
+            
+            <Divider className='custom-divider'/>
 
             <Typography variant='h4'component='h4' className='box-s-heading'>
               Step 3
             </Typography>
             <p className='box-text'>Click Next to continue.</p>
-            <img src={require('../assets/java/install-step3.png')} alt='step-3' className='screenshotd'/>
+            <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step3.png')} alt='step-3' className='screenshote'/>
 
+            <Divider className='custom-divider'/>
+
+            <p className='box-text'>Accept the terms and click on Next.</p>
+            <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step3-1.png')} alt='step-3-1' className='screenshote'/>
+
+            <Divider className='custom-divider'/>
+
+            <p className='box-text'>Click on <i>Complete</i> button for Complete Installation or otherwise for custom features.</p>
+            <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step3-2.png')} alt='step-3-2' className='screenshote'/>
+
+            <Divider className='custom-divider'/>
+
+            <p className='box-text'>Check <i>Install MongoD as a Service</i> and select <i>Run service as Network Service user</i> and click on Next.</p>
+            <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step3-3.png')} alt='step-3-3' className='screenshote'/>
+
+            <Divider className='custom-divider'/>
+            
+            <p className='box-text'>Check <i>Install MongoDB Compass</i> and click on Next.</p>
+            <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step3-4.png')} alt='step-3-4' className='screenshote'/>
+
+            <Divider className='custom-divider'/>
+            
+            <p className='box-text'>Click on <i>Install</i> to begin the installation,</p>
+            <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step3-5.png')} alt='step-3-5' className='screenshote'/>
+
+            <Divider className='custom-divider'/>
+            
+            <p className='box-text'>and wait until the installation is complete.</p>
+            <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step3-7.png')} alt='step-3-7' className='screenshote'/>
+
+            <Divider className='custom-divider'/>
+            
+            <p className='box-text'>Now, click on Finish to exit the Setup Wizard.</p>
+            <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step3-8.png')} alt='step-3-8' className='screenshote'/>
+
+            <Divider className='custom-divider'/>
 
             <Typography variant='h4'component='h4' className='box-s-heading'>
               Step 4
             </Typography>
-            <p className='box-text'>Browse the desired location to store and click on Next.</p>
-            <img src={require('../assets/java/install-step4.png')} alt='step-4' className='screenshotd'/>
 
-            <Typography variant='h4'component='h4' className='box-s-heading'>
-              Step 5
-            </Typography>
-            <p className='box-text'>Click on Install to begin the installation, </p>
-            <img src={require('../assets/java/install-step5.png')} alt='step-5' className='screenshotd'/>
-            <p className='box-text'>and wait until the installation is completed</p>
-            <img src={require('../assets/java/install-step6.png')} alt='step-5' className='screenshotd'/>
-            <p className='box-text'>and then, click on Next.</p>
+            <p className='box-text'>Open MongoDB Compass.</p>
+            <img src={require('../assets/mongo/compassandmongodbcommunity/setup-step4.png')} alt='step-4' className='screenshots'/>
 
-            <Typography variant='h4'component='h4' className='box-s-heading'>
-              Step 6
-            </Typography>
-            <p className='box-text'>Now, click on Finish to exit the Setup Wizard. You have successfully install Amazon Corretto JDK 8.</p>
-            <img src={require('../assets/java/install-step7.png')} alt='step-6' className='screenshotd'/>
-
-            <Typography variant='h3'component='h3' className='box-heading'>
-              Verification
-            </Typography>
-
-            <p className='box-text'>In the command prompt, type in <i>java -version</i> and run it.</p>
-            <img src={require('../assets/java/verify-step7.png')} alt='step-7' className='screenshots' />
-            <img src={require('../assets/java/verify-step8.png')} alt='step-8' className='screenshots' />
-            <p className='box-text'>You have successfully installed Java 8.</p>
+            <p className='box-text'>You have successfully installed MongoDB Compass.</p>
 
           </div>
         </div>
