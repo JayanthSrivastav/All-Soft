d4dec1de4f2e361a0dcb141c81e669c0e8b1994b
JayanthSrivastav <66516111+JayanthSrivastav@users.noreply.github.com>
2024-10-17 02:13:14 +0530

    Added Search bar

--- src/subs/Home.jsx
+++ src/subs/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Grid from './Card';
+import React from 'react';
+import BasicCard from './Card';
+import SearchBar from './SearchBar';
 // import Navbar from './Navbar';
 
 const Home = () => {
@@ -9,10 +10,12 @@ const Home = () => {
             <div className="Heading"><h1>Welcome home.</h1></div>
             <div className="Subheading">Well, every software and stuff.</div>
 
+            
+            <div><SearchBar /></div>
             {/* Let's use stepper */}
 
             {/* <Grid className="gridStyle" /> */}
-            <Grid className="gridStyle" />
+            <BasicCard className="gridStyle" />
         </div>
 
         
--- src/subs/SearchBar.jsx
+++ src/subs/SearchBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { Fab, IconButton, Paper, TextField } from '@mui/material';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div className='search-wrapper'>
+        {/* <TextField fullWidth></TextField>
+        <Fab><SearchIcon color="primary" fontSize='large'/></Fab> */}
+        <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}>
+            <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search" inputProps={{ 'aria-label': 'search-bar' }} />
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon />
+            </IconButton>
+        </Paper>
+    </div>
   )
 }
 
