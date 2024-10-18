435e13de52968c10b1c74b80aebfd43096d421a9
JayanthSrivastav <66516111+JayanthSrivastav@users.noreply.github.com>
2024-10-19 00:15:20 +0530

    Update NotFound.jsx

--- src/subs/NotFound.jsx
+++ src/subs/NotFound.jsx
@@ -1,8 +1,17 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const NotFound = () => {
   return (
-    <div><h1>404 - Page not found.</h1></div>
+    <>
+    <div className='not-found'>
+
+      <Typography variant='h1' component={'h1'}>404</Typography>
+      <Typography variant='h3' component={'h2'}>Not Found</Typography>
+      <Typography variant='h6' component={'h2'}>The page you're trying to access does not exist.</Typography>
+
+    </div>
+    </>
   )
 }
 
