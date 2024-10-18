101a9917f263ab5fbc710fe4e07347b8011eb5a8
JayanthSrivastav <66516111+JayanthSrivastav@users.noreply.github.com>
2024-10-18 23:32:48 +0530

    Update Home.jsx

--- src/subs/Home.jsx
+++ src/subs/Home.jsx
@@ -1,32 +1,8 @@
-// import React from 'react';
-// import BasicCard from './Card';
-// import SearchBar from './SearchBar';
-// // import Navbar from './Navbar';
-
-// const Home = () => {
-//     return (
-//         <div className="App-header">
-//             {/* <Navbar className="nav"/> */}
-//             <div className="Heading"><h1>all-soft</h1></div>
-//             <div className="Subheading">An installation guide to every programming software and tool I use.</div>
-
-            
-//             <div><SearchBar onSearch={handleSearch} /></div>
-//             {/* Let's use stepper */}
-
-//             {/* <Grid className="gridStyle" /> */}
-//             <BasicCard className="gridStyle" />
-//         </div>
-
-        
-//     )
-// }
-
-// export default Home
-
 import React, { useState } from 'react';
 import BasicCard from './Card';
 import SearchBar from './SearchBar';
+import '../App.css';
+import Footer from './Footer';
 
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -36,14 +12,23 @@ const Home = () => {
   };
 
   return (
+    <>
     <div className="App-header">
-      <div className="Heading"><h1>all-soft</h1></div>
-      <div className="Subheading">An installation guide to every programming software and tool I use.</div>
-      <div>
-        <SearchBar onSearch={handleSearch} /> 
+      <div className='App-home'>
+        <div className="Heading"><h1>all<span style={{ color: '#FFFDD0' }}>soft</span></h1></div>
+        <div className="Subheading">An installation guide to every programming software and tool I use.</div>
+        <div>
+          <SearchBar onSearch={handleSearch} /> 
+        </div>
       </div>
-      <BasicCard searchTerm={searchTerm} />
+      <div className="card-container">
+        <BasicCard searchTerm={searchTerm} />
+      </div>
     </div>
+    <div className='App-footer'>
+      <Footer className='App-footer'/>
+    </div>
+    </>
   );
 }
 
