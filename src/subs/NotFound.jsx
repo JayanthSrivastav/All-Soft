b480621644906f89f1dba41a8f10a5b82fe1a443
JayanthSrivastav <66516111+JayanthSrivastav@users.noreply.github.com>
2024-10-19 00:18:44 +0530

    Update NotFound.jsx

--- src/subs/NotFound.jsx
+++ src/subs/NotFound.jsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import React from 'react'
 
 const NotFound = () => {
@@ -6,9 +6,10 @@ const NotFound = () => {
     <>
     <div className='not-found'>
 
-      <Typography variant='h1' component={'h1'}>404</Typography>
-      <Typography variant='h3' component={'h2'}>Not Found</Typography>
+      <Typography variant='h2' component={'h1'}>404</Typography>
+      <Typography variant='h3' component={'h2'}>Page Not Found</Typography>
       <Typography variant='h6' component={'h2'}>The page you're trying to access does not exist.</Typography>
+      <Button href='/'>Go back home</Button>
 
     </div>
     </>
