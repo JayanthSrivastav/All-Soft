4d4fb70c44fe593f4df9b27e9ca5f7e9c5bb20ad
JayanthSrivastav <66516111+JayanthSrivastav@users.noreply.github.com>
2024-10-20 01:06:36 +0530

    Update Card.jsx

--- src/subs/Card.jsx
+++ src/subs/Card.jsx
@@ -11,77 +11,92 @@ const BasicCard = ({ searchTerm }) => {
 		title: 'ngrok',
 		image: 'http://www.endtoend.ai/assets/blog/tutorial/ngrok-ssh-forwarding/ssh_ngrok.jpg',
 		link: '/ngrok',
-		description: 'A tool for creating secure tunnels to your localhost.' 
+		description: 'A tool for creating secure tunnels to your localhost.',
+		category: 'Software' 
 	},
 	{ title: 'Java',
 		image: 'https://www.oracle.com/img/tech/cb88-java-logo-001.jpg',
 		link: '/java',
-		description: 'A popular programming language used for building applications.' 
+		description: 'A popular programming language used for building applications.',
+		category: 'Software' 
 	},
 	{ title: 'Maven',
 		image: 'https://user-images.githubusercontent.com/88981/188587693-5710e58b-c1ad-442c-ac06-e1aeab30a905.png',
 		link: '/maven',
-		description: 'A build automation tool used primarily for Java projects.' 
+		description: 'A build automation tool used primarily for Java projects.',
+		category: 'Software' 
 	},
 	{ title: 'Tomcat',
 		image: 'https://nsfocusglobal.com/wp-content/uploads/2019/04/apachetomcat.jpg',
 		link: '/tomcat',
-		description: 'An open-source implementation of the Java Servlet and JavaServer Pages technologies.' 
+		description: 'An open-source implementation of the Java Servlet and JavaServer Pages technologies.',
+		category: 'Software' 
 	},
 	{ title: 'Spring Boot',
 		image: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1636832404785/mTXlsmro-.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp',
 		link: '/spring-boot',
-		description: 'A framework that simplifies the development of Java applications.' 
+		description: 'A framework that simplifies the development of Java applications.',
+		category: 'Software' 
 	},
 	{ title: 'MongoDB',
 		image: 'https://webimages.mongodb.com/_com_assets/cms/kuzt9r42or1fxvlq2-Meta_Generic.png',
 		link: '/mongoDB',
-		description: 'A NoSQL database that uses a document-oriented data model.' 
+		description: 'A NoSQL database that uses a document-oriented data model.',
+		category: 'Software' 
 	},
 	{ title: 'Postman',
 		image: 'https://miro.medium.com/max/894/1*0jyROv8ksMb6tZtlf2ewVQ.png',
 		link: '/postman',
-		description: 'A powerful tool for API development and testing.' 
+		description: 'A powerful tool for API development and testing.',
+		category: 'Software' 
 	},
 	{ title: 'Node.js',
 		image: 'https://www.devteam.space/wp-content/uploads/2022/05/nodejs.jpg',
 		link: '/nodejs',
-		description: 'A JavaScript runtime built on Chrome\'s V8 engine.' 
+		description: 'A JavaScript runtime built on Chrome\'s V8 engine.',
+		category: 'Software' 
 	},
 	{ title: 'Visual Studio Code',
 		image: 'https://code.visualstudio.com/opengraphimg/opengraph-blog.png',
 		link: '/vsc',
-		description: 'A source-code editor developed by Microsoft.' 
+		description: 'A source-code editor developed by Microsoft.',
+		category: 'Software' 
 	},
 	{ title: 'GitHub',
 		image: 'https://miro.medium.com/max/1400/1*SSRjtoQ0H2X3SBPOiJ5rZw.jpeg',
 		link: '/github',
-		description: 'A platform for version control and collaboration.' 
+		description: 'A platform for version control and collaboration.',
+		category: 'Software' 
 	},
 	{ title: 'React',
 		image: 'https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124',
 		link: 'https://create-react-app.dev/',
-		description: 'A JavaScript library for building user interfaces.' 
+		description: 'A JavaScript library for building user interfaces.',
+		category: 'Software' 
 	},
 	{ title: 'Bootstrap',
 		image: 'https://getbootstrap.com/docs/5.2/assets/img/bootstrap-icons.png',
 		link: 'https://getbootstrap.com/',
-		description: 'A popular front-end framework for developing responsive websites.' 
+		description: 'A popular front-end framework for developing responsive websites.',
+		category: 'Software' 
 	},
 	{ title: 'Material UI',
 		image: 'https://cdn-media-1.freecodecamp.org/images/1*FDNeKIUeUnf0XdqHmi7nsw.png',
 		link: 'https://mui.com/material-ui/',
-		description: 'A React component library that implements Google\'s Material Design.' 
+		description: 'A React component library that implements Google\'s Material Design.',
+		category: 'Software' 
 	},
 	{ title: 'Axios',
 		image: 'https://linuxhint.com/wp-content/uploads/2022/01/word-image-1086.png',
 		link: 'https://www.npmjs.com/package/axios',
-		description: 'A promise-based HTTP client for the browser and Node.js.' 
+		description: 'A promise-based HTTP client for the browser and Node.js.',
+		category: 'Software' 
 	},
 	{ title: 'npm commands',
 		image: 'https://miro.medium.com/max/1400/1*sO5oyZhvbI0cW3SrZ9ZwCA.png',
 		link: 'https://docs.npmjs.com/cli/v6/commands',
-		description: 'Command-line tools for managing Node.js packages.' 
+		description: 'Command-line tools for managing Node.js packages.',
+		category: 'Software' 
 	},
 	];
 
