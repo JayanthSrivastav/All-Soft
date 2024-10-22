69b889234f185821eddc16248a01dff6c891f621
JayanthSrivastav <66516111+JayanthSrivastav@users.noreply.github.com>
2024-10-22 17:54:57 +0530

    Update Footer.jsx

--- src/subs/Footer.jsx
+++ src/subs/Footer.jsx
@@ -1,51 +1,61 @@
-import React from 'react';
+import React from "react";
 import { 
-  Box, 
-  Container, 
-  Typography, 
-  IconButton,
-  // useTheme,
-} from '@mui/material';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+	Box,
+	Container,
+	Typography,
+	IconButton,
+} from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
-const Footer = ({ 
-  socialLinks = {
-    linkedin: "https://linkedin.com",
-    github: "https://github.com/JayanthSrivastav/All-Soft"
-  }
+const Footer = ({
+	socialLinks = {
+		linkedin: "https://linkedin.com/in/jayanthsrivastav",
+		github: "https://github.com/JayanthSrivastav/",
+	},
 }) => {
-  // const theme = useTheme();
-
-
-  return (
-    <Box 
-      component="footer" 
-      sx={{
-        backgroundColor: '#E1DCD8',//theme.palette.background.paper,
-        p: 4,
-        mt: 'auto',
-      }}
-    >
-      <Container maxWidth="lg">
-        <Box mt={1}>
-          <Typography variant="body2" color="text.primary" align="center">
-            {"Copyright © Jayanth Srivastav "}
-            {new Date().getFullYear()}
-            {"."}
-          </Typography>
-        </Box>
-        <Box mt={2} mb={1} display="flex" justifyContent="center">
-          <IconButton aria-label="github" color="secondary" component="a" href={socialLinks.github} target='_blank' rel="noopener noreferrer">
-            <GitHubIcon />
-          </IconButton>
-          <IconButton aria-label="linkedin" color="primary" component="a" href={socialLinks.linkedin}  target='_blank' rel="noopener noreferrer">
-            <LinkedInIcon />
-          </IconButton>
-        </Box>
-      </Container>
-    </Box>
-  );
+	return (
+		<Box
+			component="footer"
+			sx={{
+				backgroundColor: "#E1DCD8",
+				p: 4,
+				mt: "auto",
+			}}>
+			<Container maxWidth="lg">
+				<Box mt={1}>
+					<Typography
+						variant="body2"
+						color="text.primary"
+						align="center">
+						{"Copyright © Jayanth Srivastav "}
+						{new Date().getFullYear()}
+						{"."}
+					</Typography>
+				</Box>
+				<Box mt={2} mb={1} display="flex" justifyContent="center">
+					<IconButton
+						aria-label="github"
+						color="secondary"
+						component="a"
+						href={socialLinks.github}
+						target="_blank"
+						rel="noopener noreferrer">
+						<GitHubIcon />
+					</IconButton>
+					<IconButton
+						aria-label="linkedin"
+						color="primary"
+						component="a"
+						href={socialLinks.linkedin}
+						target="_blank"
+						rel="noopener noreferrer">
+						<LinkedInIcon />
+					</IconButton>
+				</Box>
+			</Container>
+		</Box>
+	);
 };
 
 export default Footer;
