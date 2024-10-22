4386a781ca5d30ecde23a0bab2a6112dafcb8d0f
JayanthSrivastav <66516111+JayanthSrivastav@users.noreply.github.com>
2024-10-22 17:55:02 +0530

    Update Home.jsx

--- src/subs/Home.jsx
+++ src/subs/Home.jsx
@@ -1,35 +1,40 @@
-import React, { useState } from 'react';
-import BasicCard from './Card';
-import SearchBar from './SearchBar';
-import '../App.css';
-// import Footer from './Footer';
+import React, { useState } from "react";
+import BasicCard from "./Card";
+import SearchBar from "./SearchBar";
+import "../App.css";
 
 const Home = ({ selectedPage }) => {
-  const [searchTerm, setSearchTerm] = useState("");
+	const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSearch = (term) => {
-    setSearchTerm(term);
-  };
+	const handleSearch = (term) => {
+		setSearchTerm(term);
+	};
 
-  return (
-    <>
-    <div className="App-header">
-      <div className='App-home'>
-        <div className="Heading"><h1>all<span style={{ color: '#FFFDD0' }}>soft</span></h1></div>
-        <div className="Subheading">An installation guide to every programming software and tool I use.</div>
-        <div>
-          <SearchBar onSearch={handleSearch} /> 
-        </div>
-      </div>
-    </div>
-    <div className="card-container">
-      <BasicCard searchTerm={searchTerm} selectedPage={selectedPage} />
-    </div>
-    {/* <div className='App-footer'>
-      <Footer/>
-    </div> */}
-    </>
-  );
-}
+	return (
+		<div className="Home">
+			<div className="App-header">
+				<div className="App-home">
+					<div className="Heading">
+						<h1>
+							all<span style={{ color: "#FFFDD0" }}>soft</span>
+						</h1>
+					</div>
+					<div className="Subheading">
+						An installation guide to every programming software and tool I use.
+					</div>
+					<div>
+						<SearchBar onSearch={handleSearch} />
+					</div>
+				</div>
+			</div>
+			<div className="card-container">
+				<BasicCard
+					searchTerm={searchTerm}
+					selectedPage={selectedPage}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default Home;
